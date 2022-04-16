18b7c4bac510b9ba86415d798837cbcacbd39d99
Tony Miceli <tony@tonymiceli.com>
2022-04-16 16:33:12 -0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+        accusantium quae, corrupti et maiores dignissimos laboriosam nemo
+        sapiente impedit, saepe provident illo quibusdam! Incidunt commodi sequi
+        totam, ut magni accusamus! Distinctio debitis nemo laborum temporibus
+        maxime mollitia, aspernatur magnam! Commodi!
+      </p>
+    </section>
+  );
+};
+
+export default About;
